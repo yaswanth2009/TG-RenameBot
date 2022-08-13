a52faca246cf4b145187e987602af1697abd42d5
yaswanth2009 <108663991+yaswanth2009@users.noreply.github.com>
2022-08-13 18:59:22 +0530

    Update config.py

--- root/config.py
+++ root/config.py
@@ -9,13 +9,13 @@ import os
 
 
 class Config(object):
-    APP_ID = int(os.environ.get("APP_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
+    APP_ID = int(os.environ.get("APP_ID","11253846"))
+    API_HASH = os.environ.get("API_HASH","8db4eb50f557faa9a5756e64fb74a51a")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN","5332419326:AAGz2Nwo8ZPGa4LusUuaPtdOU2_ibvqMGSA")
     AUTH_USERS = [int(x) for x in os.environ.get("AUTH_USERS", "").split()]
     DOWNLOAD_LOCATION = "./bot/DOWNLOADS"
     DB_URI = os.environ.get("DATABASE_URL")
     # owner is for log cmd only owner can use (this can be multiple users)
-    OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID").split(" ")]
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "BotDunia")
+    OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID").split("883128927")]
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "Yaswanth_venkata_naga_sai")
     CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", False)
